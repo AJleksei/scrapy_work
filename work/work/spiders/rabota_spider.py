4bc0c4ce19d1c72cd583e2e03cce3ae0fe3c5b0e
AJleksei <AJleksei@ukr.net>
2016-03-06 18:17:20 +0200

    init commit

---
+++ work/work/spiders/rabota_spider.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import urllib
+import re
+
+from scrapy import Spider, Selector, Request
+
+from work.items import WorkItem
+
+last_page_re = re.compile(r'<a .+>(.+)<\/a>')
+
+class RabotaSpider(Spider):
+    name = 'rabota'
+    start_urls = [u'http://rabota.ua/jobsearch/vacancy_list']
+
+    def __init__(self, searchterm='', *args, **kwargs):
+        super(RabotaSpider, self).__init__(*args, **kwargs)
+        self.start_urls = ['{}?keyWords={}'.format(self.start_urls[0], searchterm)]
+
+    def parse(self, response):
+        pages = response.xpath('//tr[@class="vlp"]/td/dl/dd').extract()
+        last_page = int(last_page_re.findall(pages[-2])[0])
+        last_page = 5
+        if last_page:
+            for page in range(1, last_page+1):
+                url = u'{}&pg={}'.format(self.start_urls[0], page)
+                yield Request(url, self.parse_page)
+
+    def parse_page(self, response):
+        for v in response.xpath('//td/div[1]'):
+            item = WorkItem()
+            item['vacancy'] = v.xpath('div/h3/a/text()').extract()[0].strip()
+            item['link'] = v.xpath('div/h3/a/@href').extract()[0]
+            salary = v.xpath('div/div/b/text()').extract()
+            if salary:
+                item['salary'] = salary[0]
+            else:
+                item['salary'] = ''
+
+            yield item
---
+++ work/work/spiders/work_spider.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+import urllib
+import re
+
+from datetime import datetime
+
+import scrapy
+from work.items import WorkItem
+from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
+
+response_url_re = re.compile(r'page=(\d+)')
+
+class WorkSpider(scrapy.Spider):
+    name = "work"
+    allowed_domains = ["work.ua"]
+    start_urls = [
+        "http://www.work.ua/"
+    ]
+
+    #urls = 'программист'
+
+    def __init__(self, searchterm='', *args, **kwargs):
+        super(WorkSpider, self).__init__(*args, **kwargs)
+        self.start_urls = ['{}jobs-{}/'.format(self.start_urls[0], searchterm)]
+
+    """
+    def start_requests(self):
+        self.start_urls[0] = '{}{}/?days=125&page=1'\
+            .format(self.start_urls, urllib.quote_plus(self.urls))
+        yield self.make_requests_from_url(self.start_urls[0])
+    """
+    def parse(self, response):
+        pages = response.xpath('//*[@id="center"]/div/div/div/nav/ul/li[last()-1]/a/text()').extract()
+        if pages:
+            pages = int(pages[0])
+        pages = 5
+        for i in range(1, pages):
+            url = '{}?days=125&page={}'.format(self.start_urls[0], i)
+            yield scrapy.Request(url, callback=self.parse_contents)
+
+    def parse_contents(self, response):
+        # page = response_url_re.findall(response._url)[0]
+        elements = response.css('#center > div > div.row > '
+                                'div.col-md-8.col-left > div.card-hover')
+
+        for element in elements:
+            work_item = WorkItem()
+            work_item['vacancy'] = element.xpath('h2/a/text()').extract()[0].strip()
+            work_item['link'] = element.xpath('h2/a/@href').extract()[0].strip()
+            work_item['salary'] = ''
+            salary = element.xpath('h2/span/span[2]/text()').extract()
+            if salary:
+                work_item['salary'] = salary[0].strip()
+            print u'{} - {}'.format(work_item['salary'], work_item['vacancy'])
+            yield work_item
+
+
+        """
+        filename = 'test.txt'
+        with open(filename, 'a') as f:
+            f.write(res_all)
+        """
+        # work_item = WorkItem()
+        # work_item.vacancy = response.xpath('//*[@id="center"]/div/div/div/nav/ul/li[last()-1]/a/text()').extract()
+
+
+        """
+        filename = response.url.split("/")[-2] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        """
+
+    """
+    def parse_dir_contents(self, response):
+        for sel in response.xpath('//ul/li'):
+            item = DmozItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
+
+
+    def parse(self, response):
+        for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+
+    def parse_dir_contents(self, response):
+        for sel in response.xpath('//ul/li'):
+            item = DmozItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
+            """
+
+
+"""
+    def parse_articles_follow_next_page(self, response):
+        for article in response.xpath("//article"):
+            item = ArticleItem()
+
+            ... extract article data here
+
+            yield item
+
+        next_page = response.css("ul.navigation > li.next-page > a::attr('href')")
+        if next_page:
+            url = response.urljoin(next_page[0].extract())
+            yield scrapy.Request(url, self.parse_articles_follow_next_page)
+"""
